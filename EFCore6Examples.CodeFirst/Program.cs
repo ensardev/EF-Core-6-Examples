bf1337db279701fc3fa56820f14aa432c1035935
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-02-22 01:04:09 +0300

    Add new console project and set DbContext configuration for CodeFirst example.

---
+++ EFCore6Examples.CodeFirst/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EFCore6Examples.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"Ürün : {p.Name} - Fiyat : {p.Price} - Stok : {p.Stock}");
+    });
+}
