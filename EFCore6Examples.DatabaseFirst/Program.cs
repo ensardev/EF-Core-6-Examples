c412dedef2eb97bbd541560e9ac6073037d21d24
Ensar Akkuzey <ensarakkuzey@gmail.com>
2023-02-21 22:30:11 +0300

    Add project files.

---
+++ EFCore6Examples.DatabaseFirst/Program.cs
@@ -0,0 +1,14 @@
+﻿using EFCore6Examples.DatabaseFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext(DbContextInitializer.OptionsBuilder.Options))
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine(p.Name + " - - - Fiyat : " + p.Price);
+    });
+}
